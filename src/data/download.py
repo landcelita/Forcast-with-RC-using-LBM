09c2310ad4c737d11ca549076f2ca5d23078cc70
yamakura <yamakura@heartland-iti.co.jp>
2021-12-07 23:51:41 +0900

    functionalized download.py

--- src/data/download.py
+++ src/data/download.py
@@ -6,65 +6,101 @@ import subprocess
 
 load_dotenv(override=True)
 
+PREFIX = "Z__C_RJTD_"
+SUFFIX = "0000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin"
+PRE_URL = "http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/original/"
+YEAR = 2020
+
+# eg. 
+# http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/original
+#   /2020/01/01/
+#   Z__C_RJTD_20200101210000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin
+
+START = date(YEAR, 1, 1)
+STOP = date(YEAR+1, 1, 1) # Don't download huge data at once not to place burden on the server.
+SAVE_DIR = os.getenv("GRIBFILE_SAVE_DIR") + str(YEAR) + "/"
+MAIN_DATA_DIR = os.getenv("DATA_DIR") + str(YEAR) + "/"
+
+os.chdir(SAVE_DIR)
+
 def date_range(start, stop, step=timedelta(days=1)):
     current = start
     while current < stop:
         yield current
         current += step
 
+def download_gribfiles(d, h, condition):
+    """
+    Download grib files in GRIBFILE_SAVE_DIR, 
+    extracting from it and only saving data
+    that match the condition.
+
+    Args:
+        d (date) : the date of grib data you want to get.
+        h (int) : the hour of grib data you want to get.
+        condition (str) : the condition of grib data you want to get.
+    """
+    time_str = f"{d:%Y%m%d}" + str(h).zfill(2)
+    filename = PREFIX + time_str + SUFFIX
+    url = PRE_URL + f"{d:%Y}/{d:%m}/{d:%d}/" + filename
+    wget.download(url)
+
+    result = subprocess.Popen(
+        ["wgrib2", filename, "-match", condition],
+        stdout=subprocess.PIPE
+    )
+    out = result.communicate()[0].decode('utf-8').splitlines()
+    submsg_count = len(out)
+
+    subprocess.run(
+        ["wgrib2", filename, "-for_n", "1:"+str(submsg_count),\
+            "-grib", time_str + condition + '.grib2']
+    )
+
+    os.remove(filename)
+
+def extract_main_data(d, h, condition, datatypes):
+    """
+    Extracting main data from grib files in GRIBFILE_SAVE_DIR
+    and saving in OUTPUT_DIR. The datatypes of main data are 
+    specified by args.
+
+    Args:
+        d (date) : the date of grib data you want to extract.
+        h (int) : the hour of grib data you want to extract.
+        condition (str) : the suffix (condition) of data filename
+            you want to extract from.
+        datatypes (list) : the datatypes of grib data you want to extract.
+    """
+    time_str = f"{d:%Y%m%d}" + str(h).zfill(2)
+    filename = time_str + condition + '.grib2'
+
+    ids = []
+    for datatype in datatypes:
+        result = subprocess.Popen(
+            ["wgrib2", filename, "-match", datatype],
+            stdout=subprocess.PIPE
+        )
+        out = result.communicate()[0].decode('utf-8')
+        id = out.split(':')[0]
+        ids.append(id)
+
+    ids_str = '|'.join(ids)
+    save_path = MAIN_DATA_DIR + time_str + ".grib2"
+
+    subprocess.run(
+        ["wgrib2", filename, "-match", "^(" + ids_str + "):", "-grib", save_path]
+    )
+
+
 def main():
-    PREFIX = "Z__C_RJTD_"
-    SUFFIX = "0000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin"
-    PRE_URL = "http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/original/"
-
-    # eg. 
-    # http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/gpv/original
-    #   /2020/01/01/
-    #   Z__C_RJTD_20200101210000_MSM_GPV_Rjp_Lsurf_FH00-15_grib2.bin
-
-    START = date(2020, 9, 9)
-    STOP = date(2021, 1, 1) # Don't download huge data at once not to place burden on the server.
-    OUTPUT_DIR = os.getenv("OUTPUT_DIR") + "2020"
-
-    DATATYPES = ["PRES", "UGRD", "VGRD"]
-
-    os.chdir(OUTPUT_DIR)
+    datatypes = ["PRMSL", "UGRD", "VGRD"]
 
     for d in date_range(START, STOP):
         for h in range(0, 24, 3):
-            time_str = f"{d:%Y%m%d}" + str(h).zfill(2)
-            filename = PREFIX + time_str + SUFFIX
-            url = PRE_URL + f"{d:%Y}/{d:%m}/{d:%d}/" + filename
-            wget.download(url)
+            download_gribfiles(d, h, "anl")
+            extract_main_data(d, h, "anl", datatypes)
 
-            for datatype in DATATYPES:
-                result = subprocess.Popen(["wgrib2", filename, "-match", datatype, "-match", "anl"], \
-                            stdout=subprocess.PIPE)
-                out = result.communicate()[0].decode('utf-8')
-                id = out.split(':')[0]
-                subprocess.run(["wgrib2", filename, '-d', id, '-grib', time_str + datatype + '.grib2'])
-
-            grib_files = [time_str + datatype + '.grib2' for datatype in DATATYPES]
-            merged_filepath = time_str + "_merged.grib2"
-            
-            with open(merged_filepath, 'wb') as saveFile:
-                for f in grib_files:
-                    data = open(f, "rb").read()
-                    saveFile.write(data)
-                    saveFile.flush()
-
-            for datatype in DATATYPES:
-                os.remove(time_str + datatype + '.grib2')
-            os.remove(filename)
-
-
-
-
-
-            
-            
-        
-            
 
 if __name__ == '__main__':
     main()
