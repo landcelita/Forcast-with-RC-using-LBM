ce4e4f8acbec8da3d850d68673278bd9ccf6b0d9
yamakura <yamakura@heartland-iti.co.jp>
2022-01-20 17:25:07 +0900

    datatolbm.py

--- src/visualize/data_to_lbm.py
+++ src/visualize/data_to_lbm.py
@@ -6,13 +6,14 @@ mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 sys.path.append("../data")
+import numpy as np
 from extract import fetchdata
 
 fig = plt.figure()
 
 DATA = 2020100200
 dx = 5600
-dt = 10
+dt = 5
 c = dx / dt
 v_real = 0.000015
 viscosity = v_real * dt / dx**2
@@ -20,20 +21,21 @@ print(viscosity)
 
 pressure, wind_speed  = fetchdata(DATA)
 
-lbm = LBM(pressure.shape, viscosity=viscosity)
+lbm = LBM(pressure.shape, dt=dt)
 lbm.rho = pressure
 lbm.u = wind_speed / c
-im = plt.imshow(lbm.rho, cmap='jet', interpolation='nearest', animated=True)
+im = plt.imshow(np.sqrt(wind_speed[0]**2 + wind_speed[1]**2), cmap='jet',\
+                     interpolation='nearest', animated=True)
 
 def update(*args):
     lbm.forward_a_step()
-    pressure = lbm.rho
+    wind_speed = np.sqrt((lbm.u[0]*c)**2 + (lbm.u[1]*c)**2)
 
-    im.set_array(pressure)
+    im.set_array(wind_speed)
     return im,
 
 ani = animation.FuncAnimation(fig, update, interval=12, frames=1000)
 
-ani.save("data_to_lbm.mp4", writer="ffmpeg", fps=30, bitrate=1000)
-# plt.show()
+# ani.save("lbm_windspeed_with_coriolis.mp4", writer="ffmpeg", fps=30, bitrate=1000)
+plt.show()
 
